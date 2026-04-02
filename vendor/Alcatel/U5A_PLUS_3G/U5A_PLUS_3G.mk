12cddd0341f6294996e24e5e88ae8495a95179d7
ixi5657 <amukovars@icloud.com>
2026-04-02 11:42:48 +0300

    Create U5A_PLUS_3G.mk

---
+++ vendor/Alcatel/U5A_PLUS_3G/U5A_PLUS_3G.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+	vendor/Alcatel/U5A_PLUS_3G/lib/foo.so:/system/lib/foo.so \
+	vendor/Alcatel/U5A_PLUS_3G/lib/hw/foo.so:/system/lib/hw/foo.so \
+	vendor/Alcatel/U5A_PLUS_3G/bin/foo:/system/bin/foo \
