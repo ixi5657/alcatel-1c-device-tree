82d3500b59ca7fb5f45c57e06ff3925fbc86a8cf
ixi5657 <amukovars@icloud.com>
2026-04-02 13:48:14 +0300

    Update device.mk

--- device/Alcatel/U5A_PLUS_3G/device.mk
+++ device/Alcatel/U5A_PLUS_3G/device.mk
@@ -1,2 +1,8 @@
+LOCAL_PATH := device/U5A_PLUS_3G/U5A_PLUS_3G
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6580.rc:root/init.recovery.mt6580.rc
+
+ifeq ($(TARGET_DEVICE),U5A_PLUS_3G)
+include $(LOCAL_PATH)/Android.mk
+endif
